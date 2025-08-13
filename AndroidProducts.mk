6eff4a7070ccf5f7530fc2cde586900f2209264b
XingKongTanSuo <3329989998@qq.com>
2025-08-13 18:24:46 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_k69v1_64.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_k69v1_64-user \
+    twrp_k69v1_64-userdebug \
+    twrp_k69v1_64-eng
---
+++ twrp_k69v1_64.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from k69v1_64 device
+$(call inherit-product, device/alps/k69v1_64/device.mk)
+
+PRODUCT_DEVICE := k69v1_64
+PRODUCT_NAME := twrp_k69v1_64
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := k69v1_64
+PRODUCT_MANUFACTURER := alps
+
+PRODUCT_GMS_CLIENTID_BASE := android-alps
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_k69v1_64-user 10 QP1A.190711.020 rq0mp1k69v164P7 dev-keys"
+
+BUILD_FINGERPRINT := alps/full_k69v1_64/k69v1_64:10/QP1A.190711.020/rq0mp1k69v164P7:user/dev-keys
